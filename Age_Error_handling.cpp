46d9fac34dee42eba171f5e246eeedfdbe6e5ecd
Dev <devkdarji@gmail.com>
2025-10-03 22:15:33 +0530

    Create Age_Error_handling.cpp

---
+++ Age_Error_handling.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int age;
+    cout<<"Enter age: ";
+    cin>>age;
+    try{
+        if(age<18){
+            throw age;
+        }
+        else{
+            cout<<"Age: "<<age<<"\nAPPROVED"<<endl;
+        }
+    }
+    catch(int a){
+        cout<<"\nERROR: Underage! ("<<age<<")"<<endl;
+    }
+}
+
+//OUTPUT:
+
+/*
+Enter age: Age: 36
+APPROVED
+
+Enter age: 8
+ERROR: Underage! (8)
+*/
